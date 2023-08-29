67698add5dbd04f1aacc8a1644c25b47ae5a808a
Eliecer Alan <Eliecer.Alan@possible.com>
2023-08-29 09:03:35 -0600

    Added JavaDoc, Configurations and new test

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,6 +9,7 @@ pipeline {
     stages {
         stage ('Initialize') {
             steps {
+                slackSend (color: "good", message: "Build started: ${env.JOB_NAME} #${env.BUILD_NUMBER}")
                 sh '''
                     echo "PATH = ${PATH}"
                     echo "M2_HOME = ${M2_HOME}"
@@ -17,12 +18,28 @@ pipeline {
             }
         }
 
-        stage('Test') {
+        stage ('Test') {
             steps {
-                sh 'mvn validate'
+                slackSend (color: "good", message: "Tests started at ${env.JOB_NAME} #${env.BUILD_NUMBER}")
                 sh 'mvn test -DappEnv=ci'
             }
         }
 
+        stage ('Send Reports') {
+            steps {
+                slackSend (color: "good", message: "Tests done. Compressing reports: ${env.JOB_NAME} #${env.BUILD_NUMBER}")
+                slackUploadFile (filePath: "./reports.zip")
+                //sh zip
+            }
+        }
+
+    }
+
+    post {
+        success {
+            script {
+                slackSend(channel: "selenium-automation", message: "my-first-pipeline-slack passed successfully")
+            }
+        }
     }
 }
--- src/main/java/org/ealancas/functionaltesting/properties/ReadProperties.java
+++ src/main/java/org/ealancas/functionaltesting/properties/ReadProperties.java
@@ -8,11 +8,23 @@ import java.util.Properties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
+/**
+ * <h2>ReadProperties</h2>
+ * The ReadProperties class contains the logic to read and store
+ * the properties file according to the appEnv var passed via
+ * the test Maven command.
+ * <p>
+ *
+ * @author Eliecer Alan
+ * @author ealancascante@gmail.com
+ * @author <a href="https://github.com/e-alancas">GitHub profile</a>
+ * @version 1.0
+ * @since 2023-08-25
+ */
 public class ReadProperties {
-    private String propertiesPath = this.getPropertiesPath();
+
     private static final Logger logger = LogManager.getLogger(ReadProperties.class);
+    private String propertiesPath = this.getPropertiesPath();
     private Properties properties = new Properties();
     private static final ReadProperties instance;
 
@@ -20,6 +32,7 @@ public class ReadProperties {
         try {
             instance = new ReadProperties();
         } catch (IOException e) {
+            logger.error(e.getMessage());
             throw new RuntimeException(e);
         }
     }
@@ -39,16 +52,21 @@ public class ReadProperties {
     }
 
     public String toString() {
-        properties.forEach((key, value) -> System.out.println("Key : " + key + ", Value : " + value));
+        properties.forEach((key, value) -> logger.info("Properties.toString: Key : " + key + ", Value : " + value));
         return "";
     }
 
     private String getPropertiesPath() {
         String appEnv = System.getProperty("app.env", "local");
+
+        logger.info("Provided appEnv: " + appEnv);
+
         switch (appEnv) {
             case "ci":
+                logger.info("Set path for properties at: src/main/resources/ci/ci.properties");
                 return "src/main/resources/ci/ci.properties";
             default:
+                logger.info("Set path for properties at: src/main/resources/local/local.properties");
                 return "src/main/resources/local/local.properties";
         }
     }
